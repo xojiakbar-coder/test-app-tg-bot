6a4074357ec7afb024f6f1f29f4a407e04d1c713
Xojiakbar <xojiakbarisroilov.777@gmail.com>
2025-08-31 22:24:54 +0500

    some changed for deploy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,30 @@
-import { useMemo } from 'react';
-import { Navigate, Route, Routes, HashRouter } from 'react-router-dom';
-import { retrieveLaunchParams, useSignal, isMiniAppDark } from '@telegram-apps/sdk-react';
-import { AppRoot } from '@telegram-apps/telegram-ui';
+import { useMemo } from "react";
+import { Navigate, Route, Routes, HashRouter } from "react-router-dom";
+import {
+  retrieveLaunchParams,
+  useSignal,
+  isMiniAppDark,
+} from "@telegram-apps/sdk-react";
+import { AppRoot } from "@telegram-apps/telegram-ui";
 
-import { routes } from '@/navigation/routes.tsx';
+import { routes } from "@/navigation/routes.tsx";
 
 export function App() {
   const lp = useMemo(() => retrieveLaunchParams(), []);
   const isDark = useSignal(isMiniAppDark);
+  console.log("app is working");
 
   return (
     <AppRoot
-      appearance={isDark ? 'dark' : 'light'}
-      platform={['macos', 'ios'].includes(lp.tgWebAppPlatform) ? 'ios' : 'base'}
+      appearance={isDark ? "dark" : "light"}
+      platform={["macos", "ios"].includes(lp.tgWebAppPlatform) ? "ios" : "base"}
     >
       <HashRouter>
         <Routes>
-          {routes.map((route) => <Route key={route.path} {...route} />)}
-          <Route path="*" element={<Navigate to="/"/>}/>
+          {routes.map((route) => (
+            <Route key={route.path} {...route} />
+          ))}
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </HashRouter>
     </AppRoot>
