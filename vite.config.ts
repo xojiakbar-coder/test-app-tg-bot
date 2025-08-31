93fe8d5f911e60a4633d7eff3a24c9948ea1d4e4
Xojiakbar <xojiakbarisroilov.777@gmail.com>
2025-08-31 22:21:11 +0500

    some changes for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/test",
+  base: "/test-app-tg-bot",
   css: {
     preprocessorOptions: {
       scss: {
