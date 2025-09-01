9d5f47b12f37de1e4617f91e42649a4d90712169
Xojiakbar <xojiakbarisroilov.777@gmail.com>
2025-09-01 20:49:32 +0500

    fix:routes bug

--- src/navigation/routes.tsx
+++ src/navigation/routes.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/layouts";
 import * as RouterDom from "react-router-dom";
 
-import { InitDataPage } from "@/pages/InitDataPage";
+// import { InitDataPage } from "@/pages/InitDataPage";
 
 // passenger pages
 import NewOrder from "@/pages/Passenger/NewOrder/NewOrder";
@@ -22,7 +22,7 @@ export const routes: RouterDom.RouteObject[] = [
       { path: "/passenger-orders", element: <PassengerOrders /> },
       { path: "/passenger-profile", element: <PassengerProfile /> },
       { path: "/passenger-new-order/:id", element: <NewOrder /> },
-      { path: "/passenger-orders-archive", element: <InitDataPage /> },
+      // { path: "/passenger-orders-archive", element: < /> },
     ],
   },
   {
@@ -33,7 +33,7 @@ export const routes: RouterDom.RouteObject[] = [
       { path: "/driver/driver-orders", element: <DriverRides /> },
       { path: "/driver/driver-new-ride", element: <DriverRoutes /> },
       { path: "/driver/change-tariff", element: <DriverChangeTariff /> },
-      { path: "/driver/driver-rides-archive", element: <InitDataPage /> },
+      // { path: "/driver/driver-rides-archive", element: <InitDataPage /> },
     ],
   },
 ];
